f2cf6d1718f48c8f0baf045ffd660d9b01c47c80
sylviatang214 <143663556+sylviatang214@users.noreply.github.com>
2024-05-03 21:24:33 -0500

    Add files via upload

---
+++ CS303 Elevator/Doubly_List.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+class node {
+public:
+	node* next = nullptr;
+	node* prev = nullptr;
+
+	string direction = "\0";
+	char id = '\0'; //id of elevator that can answer calls
+	int floor = 0;
+	int waitingTime = 0;
+};
+
+class Doubly_List {
+private:
+	node* headPtr;
+	node* tailPtr;
+public:
+
+	Doubly_List();
+
+	void insert(int i, int floor, string direction);
+	void print();
+	bool empty();
+	void push_front(node* tempNode);
+	void push_back(node* tempNode);
+	void sort();
+	void pop(int floor, string direction);
+
+	int stop(int f, string d, char i);
+	void update_wait();
+	
+
+};
